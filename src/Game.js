6a5f065fbc3299b9eb7db42db05a49368866b484
David Poindexter <drpoindexter@gmail.com>
2019-11-26 17:47:51 -0500

    Move board styles into component, fix fragile class-based tests

--- src/Game.js
+++ src/Game.js
@@ -1,17 +1,6 @@
 import React from "react";
 import Board from "./Board";
 // @TODO: Implement move history
-const Game = () => (
-  <>
-    <div>
-      <Board />
-    </div>
-    <div className="game-info">
-      <p>Game Info</p>
-      <div>{/* status */}</div>
-      <ol>{/* TODO */}</ol>
-    </div>
-  </>
-);
+const Game = () => <Board />;
 
 export default Game;
