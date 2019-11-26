62eec9f606061bbb32ccc79363c821527457f55a
David Poindexter <drpoindexter@gmail.com>
2019-11-26 18:07:36 -0500

    Move game styles into game component

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,20 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
 import Board from "./Board";
 // @TODO: Implement move history
-const Game = () => <Board />;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font: 14px "Century Gothic", Futura, sans-serif;
+    margin: 20px;
+  }
+`;
+
+const Game = () => (
+  <>
+    <GlobalStyle />
+    <Board />
+  </>
+);
 
 export default Game;
