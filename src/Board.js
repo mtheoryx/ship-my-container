988bb92c7e2d757da6ed6a362f77701b15058dbb
David Poindexter <drpoindexter@gmail.com>
2019-11-26 19:58:28 -0500

    Toelint2 CSS GRID separates our display concerns

--- src/Board.js
+++ src/Board.js
@@ -12,12 +12,13 @@ const VerticalSection = styled.div`
   margin-bottom: 10px;
 `;
 
-const BoardRow = styled.div`
-  &:after {
-    clear: both;
-    content: "";
-    display: table;
-  }
+const PlayingArea = styled.div`
+  display: grid;
+  width: calc(34px * 3);
+  grid-template-areas:
+    ". . ."
+    ". . ."
+    ". . .";
 `;
 
 const Board = () => {
@@ -49,21 +50,15 @@ const Board = () => {
       <VerticalSection id="turn-order">
         Next Player: {xIsNext ? "X" : "O"}
       </VerticalSection>
-      <BoardRow>
-        <Square id="0" value={squares[0]} onClick={() => handleClick(0)} />
-        <Square id="1" value={squares[1]} onClick={() => handleClick(1)} />
-        <Square id="2" value={squares[2]} onClick={() => handleClick(2)} />
-      </BoardRow>
-      <BoardRow>
-        <Square id="3" value={squares[3]} onClick={() => handleClick(3)} />
-        <Square id="4" value={squares[4]} onClick={() => handleClick(4)} />
-        <Square id="5" value={squares[5]} onClick={() => handleClick(5)} />
-      </BoardRow>
-      <BoardRow>
-        <Square id="6" value={squares[6]} onClick={() => handleClick(6)} />
-        <Square id="7" value={squares[7]} onClick={() => handleClick(7)} />
-        <Square id="8" value={squares[8]} onClick={() => handleClick(8)} />
-      </BoardRow>
+      <PlayingArea>
+        {squares.map((square, index) => (
+          <Square
+            id={index}
+            value={square}
+            onClick={() => handleClick(index)}
+          />
+        ))}
+      </PlayingArea>
     </div>
   );
 };
